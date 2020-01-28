44039bd7afcf58377e47418741a0aff53f498cfb
fuston05 <fuston05@yahoo.com>
2020-01-27 19:28:29 -0500

    basic set up layed out, created a 'teamData' file to import from. built out basic form

---
+++ src/components/Form/Form.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Form = () => {
+  return (
+    <div className= 'formCont'>
+      <form>
+        <label>
+          <input name= 'fullName' placeholder= 'Name' type= 'text' />
+        </label> 
+
+        <label>
+          <input name= 'email' placeholder= 'Email' type= 'email' />
+        </label>
+
+        <label>
+          <input name= 'role' placeholder= 'Company Role' type= 'text' />
+        </label>       
+      </form>
+    </div>
+  )
+}
+
+export default Form;
---
+++ src/components/Team/teamData.js
@@ -0,0 +1,30 @@
+team=[
+  {
+    name: 'Tommy Tommerson',
+    email: 'ttommerson@email.com',
+    role: 'Data Scientist'
+  },
+  {
+    name: 'Scott Fuston',
+    email: 'sfuston@email.com',
+    role: 'Backend-engineer'
+  },
+  {
+    name: 'Lisa Smith',
+    email: 'lsmith@email.com',
+    role: 'Frontend Engineer'
+  },
+  {
+    name: 'Kelly Williams',
+    email: 'kwilliams@email.com',
+    role: 'UI/UX Designer'
+  },
+  {
+    name: 'Bruce Wayne',
+    email: 'batman@email.com',
+    role: 'Project Manager'
+  }
+];
+
+
+export default team;
